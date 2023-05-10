ff7d015175838d4a0c255072d352e041a11117ea
rafatrinity <rafaeltrindade649@gmail.com>
2023-05-10 15:41:41 -0300

    build: :memo:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BaseProvider, LightTheme } from "baseui";
-import { Provider as StyletronProvider } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
-
+import "./assests/font-awesome/css/all.css";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BaseProvider, LightTheme } from "baseui";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./assests/font-awesome/css/all.css";
 
 const engine = new Styletron();
 
@@ -24,4 +25,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
